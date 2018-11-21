bf3f45b492d97587efd4211ed61d09766ed5f04b
Bram va n Adrichem <302251971@student.rocmondriaan.nl>
2018-11-21 12:00:31 +0100

    Veranderingen beweging platform

--- Platform1.java
+++ Platform1.java
@@ -12,54 +12,46 @@ public class Platform1 extends Mover
      * Act - do whatever the platform wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int Teller;
     public Platform1()
     {
         getImage().mirrorVertically();
         
     }
+    
     public void act() 
     {
+        PLbewegen();
         applyVelocity();
         if(Greenfoot.isKeyDown("up"))
         {
          PLbewegen();   
         }
-        if(Greenfoot.isKeyDown("left"))
-        {
-         Bew1();   
-        }
-        if(Greenfoot.isKeyDown("right"))
-        {
-         Bew2();   
-        }
+       
     }    
     
     public void PLbewegen()
     {
-         for(int i = 0; i < 2; i ++)
+        if(Teller == 1)
         {
-            setLocation(getX()-4,getY()+2 );
-            velocityX = 2;
-            velocityY = 2;
-        }
-        for(int i = 0; i < 2; i ++)
-        {
-            setLocation(getX()+4,getY()-2);
-            velocityX = -2;
-            velocityY = -2;
+            //setLocation(getX()-2,getY()+4);
+            velocityX = -1;
+            velocityY = -1;
         }
 
+        if(Teller == 40)
+        {
+            //setLocation(getX()+2,getY()-4 );
+            velocityX = 1;
+            velocityY = 1;
+        }
+        if(Teller >= 79)
+        {
+            Teller = 0;
+        }
+        Teller ++;
     }
-    public void Bew1()
-    {
-         velocityX = 2;
-         velocityY = 2;
-    }
-    public void Bew2()
-    {
-        velocityX = -2;
-        velocityY = -2;
-    }
+    
     
 }
 
