a2d14799cb3b0205eaa9bd088256cb7df1bb8e5c
Bram va n Adrichem <302251971@student.rocmondriaan.nl>
2018-12-06 15:28:33 +0100

    map aanmpassing

--- Map2.java
+++ Map2.java
@@ -98,6 +98,7 @@ public class Map2 extends World
         // Alle objecten toevoegen aan de wereld: camera, main karakter en mogelijke enemies
         addObject(camera, 0, 0);
         addObject(hero, 390, 338);
+        addObject(new HUD(),0,0);
 
         // Initialiseren van de CollisionEngine zodat de speler niet door de tile heen kan lopen.
         // De collision engine kijkt alleen naar de tiles die de variabele solid op true hebben staan.
